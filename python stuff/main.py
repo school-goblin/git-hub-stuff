306362bd3773aaf395120cfa81e748203e2bae88
big fela <126705868+school-goblin@users.noreply.github.com>
2023-03-01 11:10:00 -0700

    yeah all of my code
    yipppeeee

---
+++ python stuff/main.py
@@ -0,0 +1,11 @@
+cool = int(input(print("how many time must you be???")))
+
+
+while cool >= 0:
+    cool -= 1
+    if cool == 69 or 420:
+        print("nice")
+    else:
+        print("hello world")
+        print("big fela")
+        print("among us")
